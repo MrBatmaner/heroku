d0297e57b7411bb25b4889c18b4a033f5b3bab55
Joker <mrbatmanerr@gmail.com>
2023-10-13 09:59:24 +1100

    envport

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
 
 db.init().then(() => {
-    app.listen(3000, () => console.log('Listening on port 3000'));
+    app.listen(process.env.PORT || 3000, () => console.log('Listening on port 3000'));
 }).catch((err) => {
     console.error(err);
     process.exit(1);
@@ -30,3 +30,5 @@ const gracefulShutdown = () => {
 process.on('SIGINT', gracefulShutdown);
 process.on('SIGTERM', gracefulShutdown);
 process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
+
+
